4baf47fd47d1b3b940ced15dad906c5734fd3d70
EMRE GÜNDOĞDU <gundogdu.emre@yahoo.com>
2019-04-19 13:54:51 +0300

    added send to the cw metric

--- platform/ssl-expire-check/sslexpirecheck.py
+++ platform/ssl-expire-check/sslexpirecheck.py
@@ -1,6 +1,9 @@
 import socket
 import ssl, boto3
 import re,sys,os,datetime
+import dateutil
+
+healthcheck_namespace = "RabbitMQ Health Check Metrics"
 
 def ssl_expiry_date(domainname):
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
@@ -10,7 +13,7 @@ def ssl_expiry_date(domainname):
         server_hostname=domainname,
     )
     # 3 second timeout because Lambda has runtime limitations
-    conn.settimeout(3.0)
+    conn.settimeout(5.0)
     conn.connect((domainname, 8883))
     ssl_info = conn.getpeercert()
     return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt).date()
@@ -33,10 +36,15 @@ def sns_Alert(dName, eDays, sslStatus, sns_topic_arn):
 
 
 ##### Main Section
-client = boto3.client('sns')
 def lambda_handler(event, context):
+    sns_client = boto3.client('sns')
+    cw_client = boto3.client('cloudwatch')
+    ssl_expire_put_cw(event,cw_client,sns_client)
+
+def ssl_expire_put_cw(event,cw,sns):
     sns_topic_arn = event["sns_topic_arn"]
     url_name = event["url_name"]
+    service_name = event["service_name"]
     print sns_topic_arn
     f = [url_name]
     for dName in f:
@@ -49,17 +57,37 @@ def lambda_handler(event, context):
         # Critical alerts
         if int(c) < 15:
             sns_Alert(dName, str(c), 'Critical', sns_topic_arn)
+            put_metric_to_cw(cw,"ServiceName", service_name, "SSLExpireDays", int(c), healthcheck_namespace)
         # First critical alert on 20 th day
         elif int(c) == 20:
             sns_Alert(dName, str(c), 'Critical', sns_topic_arn)
+            put_metric_to_cw(cw,"ServiceName", service_name, "SSLExpireDays", int(c), healthcheck_namespace)
         #third warning alert on 30th day
         elif int(c) == 30:
             sns_Alert(dName, str(c), 'Warning' ,sns_topic_arn)
+            put_metric_to_cw(cw,"ServiceName", service_name, "SSLExpireDays", int(c), healthcheck_namespace)
         #second warning alert on 40th day
         elif int(c) == 40:
             sns_Alert(dName, str(c), 'Warning', sns_topic_arn)
+            put_metric_to_cw(cw,"ServiceName", service_name, "SSLExpireDays", int(c), healthcheck_namespace)
         #First warning alert on 50th day
         elif int(c) == 50:
             sns_Alert(dName, str(c), 'Warning', sns_topic_arn)
+            put_metric_to_cw(cw,"ServiceName", service_name, "SSLExpireDays", int(c), healthcheck_namespace)
         else:
             print('Everything Fine..')
+            put_metric_to_cw(cw,"ServiceName", service_name, "SSLExpireDays", int(c), healthcheck_namespace)
+
+def put_metric_to_cw(cw,dimension_name,dimension_value,metric_name,metric_value,namespace):
+    cw.put_metric_data(
+        Namespace=namespace,
+        MetricData=[{
+            'MetricName': metric_name,
+            'Dimensions': [{
+                'Name': dimension_name,
+                'Value': dimension_value
+            }],
+            'Timestamp': datetime.datetime.now(dateutil.tz.tzlocal()),
+            'Value': metric_value
+        }]
+    )
